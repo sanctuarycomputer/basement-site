19386640e6d3b9b5ed0797070c14652af97b44b0
John Ray <john@johnraymon.com>
2018-12-13 20:25:10 -0500

    use modules for different javascript functionalities

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,26 @@
 ---
 ---
 
+// generic App class
 class App {
   constructor(container) {
     this._init(container);
     this._bindEvents();
   }
 
-  _init(container){
-    this.app = container;
-    this.subNavItems = this.app.querySelectorAll('[data-sub-nav-item]');
-    this.currentSections = this.app.querySelectorAll('[data-section]');
-    this.mobileNavIcon = this.app.querySelector('[data-mobile-nav-icon]');
-    this.mobileNav = this.app.querySelector('[data-mobile-menu]');
+  _init(container) {
+    this.appContainer = container;
+    // this.currentSections = this.appContainer.querySelectorAll('[data-section]');
+    this.mobileNavIcon = this.appContainer.querySelectorAll('[data-mobile-nav-icon]');
+    this.mobileNav = this.appContainer.querySelector('[data-mobile-menu]');
   }
 
   _bindEvents() {
-    this.subNavItems.forEach(item => {
-      item.addEventListener("click", (e) => {
-        e.preventDefault();
-        this.scrollToSection(item.getAttribute('data-sub-nav-item'))
+    this.mobileNavIcon.forEach(icon => {
+      icon.addEventListener('click', () => {
+        this.toggleMobileMenu();
       })
     })
-
-    this.currentSections.forEach(section => {
-      section.addEventListener("mouseover", () => {
-        this.toggleSubNavItem(section.getAttribute('id'))
-      })
-
-      section.addEventListener("mouseout", () => {
-        this.toggleSubNavItem(section.getAttribute('id'))
-      })
-    })
-
-    this.mobileNavIcon.addEventListener('click', () => {
-      this.toggleMobileMenu();
-    })
-  }
-
-  scrollToSection(target){
-    const section = document.getElementById(target);
-    $('html, body').animate({
-      scrollTop: $(section).offset().top - '20'
-    }, 250);
-  }
-
-  toggleSubNavItem(target) {
-    const subNavItem = Array
-      .from(this.subNavItems)
-      .find(item => item.getAttribute('data-sub-nav-item') === target)
-    if (subNavItem) $(subNavItem).toggleClass('bold');
   }
 
   toggleMobileMenu() {
@@ -57,7 +28,97 @@ class App {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+// SubNav class
+class SubNav {
+  constructor() {
+    this._init();
+    this._bindEvents();
+  }
+
+  _init() {
+    this.subNavItems = app.appContainer.querySelectorAll('[data-sub-nav-item]');
+  }
+
+  _bindEvents() {
+    this.subNavItems.forEach(item => {
+
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.scrollToSection(item.getAttribute('data-sub-nav-item'))
+      })
+    })
+  }
+
+  scrollToSection(target) {
+    const section = document.getElementById(target);
+    $('html, body').animate({
+      scrollTop: $(section).offset().top - '20'
+    }, 250);
+  }
+
+  toggleSubNavItem(target) {
+    const subNavItems = Array.from(this.subNavItems)
+    subNavItems
+      .forEach(item => $(item).removeClass('bold'))
+    const subNavItem = subNavItems
+      .find(item => item.getAttribute('data-sub-nav-item') === target);
+    if (subNavItem) $(subNavItem).toggleClass('bold');
+  }
+}
+
+
+// ScrollListener class
+class ScrollListener {
+  constructor(toggleSub) {
+    this.intializePositionMap();
+    this.bindScroll();
+    this.target = null;
+  }
+
+  intializePositionMap() {
+    this.positionsArray = [];
+    this.positionMap = Array.from(document.querySelectorAll('[data-section]')).reduce((map, element) => {
+      map[element.offsetTop] = {
+        id: element.getAttribute('id'),
+        height: element.scrollHeight
+      };
+      this.positionsArray.push(element.offsetTop);
+      return map;
+    }, {});
+  }
+
+  bindScroll() {
+    this.checkIfNewTarget();
+    document.addEventListener('scroll', this.checkIfNewTarget.bind(this));
+    document.addEventListener('resize', () => {
+      this.intializePositionMap();
+      this.checkIfNewTarget();
+    });
+  }
+
+  checkIfNewTarget() {
+    const scrollPosition = window.scrollY;
+    if (this.positionsArray) {
+      const elementPosition = this.positionsArray
+        .find(position =>
+          (position + this.positionMap[position].height) > scrollPosition
+        );
+      const element = this.positionMap[elementPosition];
+      const newTarget = element;
+      if (this.target !== newTarget) {
+        this.target = newTarget;
+        this.updateTargetElement(this.target.id);
+      }
+    }
+  }
+
+  updateTargetElement(target) {
+    console.log('called for target: ', target)
+    subNav.toggleSubNavItem(target);
+  }
+}
+
   const app_container = document.querySelector('[data-app-container]');
-  new App(app_container);
-});
+  const app = new App(app_container);
+  const subNav = new SubNav();
+  const scrollListener = new ScrollListener();
