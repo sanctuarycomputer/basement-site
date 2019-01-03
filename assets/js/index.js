eafd7e00b29a1e1374a1f874458831329b23e7d7
John Ray <john@johnraymon.com>
2019-01-03 14:14:51 -0500

    use class static props

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
 ---
 
 // Generic App Class
+
 class App {
   constructor(container) {
     this._init(container);
@@ -10,8 +11,8 @@ class App {
 
   _init(container) {
     this.appContainer = container;
-    this.mobileNavIcon = this.appContainer.querySelectorAll('[data-mobile-nav-icon]');
-    this.mobileNav = this.appContainer.querySelector('[data-mobile-menu]');
+    this.mobileNavIcon = this.appContainer.querySelectorAll(App.selectors.dataMobileNavIcon);
+    this.mobileNav = this.appContainer.querySelector(App.selectors.dataMobileMenu);
   }
 
   _bindEvents() {
@@ -23,9 +24,16 @@ class App {
   }
 
   toggleMobileMenu() {
-    this.mobileNav.classList.toggle('MobileMenu--opened');
+    this.mobileNav.classList.toggle(App.selectors.mobileMenuOpened);
   }
 }
+
+App.selectors = {
+  dataMobileNavIcon: '[data-mobile-nav-icon]',
+  dataMobileMenu: '[data-mobile-menu]',
+  mobileMenuOpened: 'MobileMenu--opened'
+}
+
 
 // SubNav Class
 class SubNav {
@@ -35,14 +43,14 @@ class SubNav {
   }
 
   _init() {
-    this.subNavItems = app.appContainer.querySelectorAll('[data-sub-nav-item]');
+    this.subNavItems = app.appContainer.querySelectorAll(SubNav.selectors.subNavItem);
   }
 
   _bindEvents() {
     this.subNavItems.forEach(item => {
       item.addEventListener("click", (e) => {
         e.preventDefault();
-        this.scrollToSection(item.getAttribute('data-sub-nav-item'))
+        this.scrollToSection(item.getAttribute(SubNav.selectors.subNavItem))
       })
     })
   }
@@ -50,13 +58,9 @@ class SubNav {
   scrollToSection(target) {
     const section = document.getElementById(target);
     const html_elem = document.querySelector('body');
-    // $('html, body').animate({
-    //   scrollTop: $(section).offset().top + 20
-    // }, 250);
-    // let timer = setInterval(function() {
-    //   if (html_elem.scrollTop == (html_elem.offsetTop + 20) ) clearInterval(timer);
-    //   else html_elem.scrollTop += 2px
-    // }, 20); // change by 2px every 20ms, about 50 frames per second
+    $('html, body').animate({
+      scrollTop: $(section).offset().top + 20
+    }, 250);
   }
 
   toggleSubNavItem(target) {
@@ -64,11 +68,14 @@ class SubNav {
     subNavItems
       .forEach(item => $(item).removeClass('bold')) // reset subnav items
     const subNavItem = subNavItems
-      .find(item => item.getAttribute('data-sub-nav-item') === target);
+      .find(item => item.getAttribute(SubNav.selectors.subNavItem) === target);
     if (subNavItem) subNavItem.classList.toggle('bold');
   }
 }
 
+SubNav.selectors = {
+  subNavItem: '[data-sub-nav-item]'
+}
 
 // ScrollListener Class
 class ScrollListener {
@@ -80,7 +87,7 @@ class ScrollListener {
 
   intializePositionMap() {
     this.positionsArray = [];
-    this.positionMap = Array.from(document.querySelectorAll('[data-section]')).reduce((map, element) => {
+    this.positionMap = Array.from(document.querySelectorAll(ScrollListener.selectors.section)).reduce((map, element) => {
       map[element.offsetTop] = {
         id: element.getAttribute('id'),
         height: element.scrollHeight
@@ -120,7 +127,11 @@ class ScrollListener {
   }
 }
 
-  const appContainer = document.querySelector('[data-app-container]');
-  const app = new App(appContainer);
-  const subNav = new SubNav();
-  const scrollListener = new ScrollListener();
+ScrollListener.selectors = {
+  section: '[data-section]'
+}
+
+const appContainer = document.querySelector('[data-app-container]');
+const app = new App(appContainer);
+const subNav = new SubNav();
+const scrollListener = new ScrollListener();
