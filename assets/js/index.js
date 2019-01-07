1455e5fb8cbe36152e656eb693e9867a3ce4da42
John Ray <john@johnraymon.com>
2019-01-07 11:20:31 -0500

    fix indentations

--- assets/js/index.js
+++ assets/js/index.js
@@ -69,21 +69,21 @@ class SubNav {
     // c = change in value
     // d = duration
     easeInOutQuad = function(t, b, c, d) {
-       t /= d/2;
-       if (t < 1) return c/2*t*t + b;
-       t--;
-       return -c/2 * (t*(t-2) - 1) + b;
+      t /= d/2;
+      if (t < 1) return c/2*t*t + b;
+      t--;
+      return -c/2 * (t*(t-2) - 1) + b;
     },
     animateScroll = function() {
-       const currentDate = +new Date();
-       const currentTime = currentDate - startDate;
-       element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
-       if(currentTime < duration) {
-           requestAnimationFrame(animateScroll);
-       }
-       else {
-           element.scrollTop = to;
-       }
+      const currentDate = +new Date();
+      const currentTime = currentDate - startDate;
+      element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+      if(currentTime < duration) {
+        requestAnimationFrame(animateScroll);
+      }
+      else {
+        element.scrollTop = to;
+      }
     };
     animateScroll();
   }
